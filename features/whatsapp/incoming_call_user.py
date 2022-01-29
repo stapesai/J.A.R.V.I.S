3a1e2034aa203aefdfeec6aaae1de328acdc1c44
codemasterayush <98602028+codemasterayush@users.noreply.github.com>
2022-01-29 13:34:34 +0530

    updated csv

--- features/whatsapp/incoming_call_user.py
+++ features/whatsapp/incoming_call_user.py
@@ -5,7 +5,8 @@ from PIL import ImageGrab
 def Caller_Name():
     attend_call_cordinates=jarvis.locateCenterOnScreen('img\call_attend.png', confidence=0.7)
     if attend_call_cordinates!=None:
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Users\swast\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+       #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\swast\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Users\bansa\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
         cap = ImageGrab.grab(bbox =(1630, 85, 1910, 130))
         ocr = pytesseract.image_to_string(cap, lang ='eng')
         return ocr
