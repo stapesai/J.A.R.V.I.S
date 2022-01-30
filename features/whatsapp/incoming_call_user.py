d3be5e79793a13956f54031145f0e1f770efd018
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-01-30 11:33:04 +0530

    Tesseract-OCR issue fixed

--- features/whatsapp/incoming_call_user.py
+++ features/whatsapp/incoming_call_user.py
@@ -5,8 +5,7 @@ from PIL import ImageGrab
 def Caller_Name():
     attend_call_cordinates=jarvis.locateCenterOnScreen('img\call_attend.png', confidence=0.7)
     if attend_call_cordinates!=None:
-       #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\swast\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Users\bansa\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' #Reinstall Tesseract-OCR in this location 'C:\Program Files\Tesseract-OCR'
         cap = ImageGrab.grab(bbox =(1630, 85, 1910, 130))
         ocr = pytesseract.image_to_string(cap, lang ='eng')
         return ocr
