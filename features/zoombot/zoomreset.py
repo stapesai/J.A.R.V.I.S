36026e8af6912c221b6e2694890affc8c4e47dde
codemasterayush <98602028+codemasterayush@users.noreply.github.com>
2022-02-01 11:50:43 +0530

    .

--- features/zoombot/zoomreset.py
+++ features/zoombot/zoomreset.py
@@ -1,11 +1,10 @@
 import pyautogui as jarvis
 def ZoomReset():
     jarvis.sleep(3)
-    jarvis.keyDown('ctrl')
-    jarvis.press('+')
-    zoom_cord = jarvis.locateCenterOnScreen('zoom_reset.png',confidence=0.8)
+    jarvis.hotkey('ctrl','+')
+    zoom_cord = jarvis.locateCenterOnScreen('edge_reset.png',confidence=0.8) or jarvis.locateCenterOnScreen('chrome_reset.png',confidence=0.8)
     if zoom_cord!=None:
         jarvis.click(zoom_cord)
-    jarvis.keyUp('ctrl')
+    
 
 ZoomReset()
