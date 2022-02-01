a5af5471309a424a593dc13d23f21b29511e440d
codemasterayush <98602028+codemasterayush@users.noreply.github.com>
2022-02-01 12:41:29 +0530

    .

--- features/zoombot/openskolaro.py
+++ features/zoombot/openskolaro.py
@@ -1,7 +1,9 @@
 import datetime
 import webbrowser
+import time
 import pyautogui as jarvis
 webbrowser.open('https://apps.skolaro.com/lecture-timetable/user')
+time.sleep(3)
 
 crnt_dateandtime=datetime.datetime.now() #diaplays date along with time
 #we want only time as skolaro automatically updates date
@@ -15,7 +17,7 @@ int_mins=int(minute)
 int_secs=int(second)
 if int_hour == 8:
     if int_mins>=20 and int_mins<=25:
-        class1_cords=jarvis.locateCenterOnScreen('firstclass.png',confidence=0.8) #for 1st class
+        class1_cords=jarvis.locateCenterOnScreen('classes\firstclass.png',confidence=0.8) #for 1st class
         if class1_cords!=None:
             print('cords founded :',class1_cords)
             jarvis.click(class1_cords)
@@ -25,8 +27,8 @@ if int_hour == 8:
         print('going on next time limit.....')
 elif int_hour == 9:
     if int_mins>=10 and int_mins<=15:
-        class2_cords=jarvis.locateCenterOnScreen('hoursize.png',confidence=0.8)
-        if class2_cords!=None:
+        class2_cords=jarvis.locateCenterOnScreen('classes\secondclass.png',confidence=0.8) #2nd class
+        if class2_cords!=None: 
             print('cords founded :',class2_cords)
             jarvis.click(class2_cords)
         else:
@@ -34,6 +36,45 @@ elif int_hour == 9:
     else:
         print('going on next time limit...')
 elif int_hour== 10:
-    if int_mins:
-        pass
-# to be continued
+    if int_mins>=10 and int_mins<=15:
+        class3_cords=jarvis.locateCenterOnScreen('classes\thirdclass.png',confidence=0.8) #3rd class
+        if class3_cords!=None: 
+            print('cords founded :',class3_cords)
+            jarvis.click(class3_cords)
+        else:
+            print('cords not found')
+    else:
+        print('going on next time limit...')
+
+elif int_hour== 11:
+    if int_mins>=0 and int_mins<=5:
+        class4_cords=jarvis.locateCenterOnScreen('classes\fourthclass.png',confidence=0.8) #4th class
+        if class4_cords!=None: 
+            print('cords founded :',class4_cords)
+            jarvis.click(class4_cords)
+        else:
+            print('cords not found')
+    else:
+        print('going on next time limit...')
+
+elif int_hour== 12:
+    if int_mins>=0 and int_mins<=5:
+        class5_cords=jarvis.locateCenterOnScreen('classes\fifthclass.png',confidence=0.8) #5th class
+        if class5_cords!=None: 
+            print('cords founded :',class5_cords)
+            jarvis.click(class5_cords)
+        else:
+            print('cords not found')
+    else:
+        print('going on next time limit...')
+
+elif int_hour== 1:
+    if int_mins>=20 and int_mins<=25:
+        class6_cords=jarvis.locateCenterOnScreen('classes\sixthclass.png',confidence=0.8) #6th class
+        if class6_cords!=None: 
+            print('cords founded :',class6_cords)
+            jarvis.click(class6_cords)
+        else:
+            print('cords not found')
+    else:
+        print('going on next time limit...')
--- features/zoombot/skolaro_extralarge.py
+++ features/zoombot/skolaro_extralarge.py
@@ -1,11 +1,13 @@
 import pyautogui as jarvis
 def Skolaro_ExtraLarge():
     jarvis.sleep(3)
-    normal_cord = jarvis.locateCenterOnScreen('skolaro_normal.png')
+    normal_cord = jarvis.locateCenterOnScreen('skolaro_normal.png',confidence=0.8)
     if normal_cord!=None:
         x=normal_cord[0]
         y=normal_cord[1]
         jarvis.click(x,y)
         jarvis.press('pagedown')
         jarvis.press('enter')
+    else:
+        print('cant find..')
 Skolaro_ExtraLarge()
