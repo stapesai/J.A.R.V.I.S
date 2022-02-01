67067d78ad71f5529a0089c31e9b2e282ee4be2e
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-02-01 11:59:02 +0530

    Update skolaro_extralarge.py

--- features/zoombot/skolaro_extralarge.py
+++ features/zoombot/skolaro_extralarge.py
@@ -1,2 +1,11 @@
+import pyautogui as jarvis
 def Skolaro_ExtraLarge():
-    
+    jarvis.sleep(3)
+    normal_cord = jarvis.locateCenterOnScreen('skolaro_normal.png')
+    if normal_cord!=None:
+        x=normal_cord[0]
+        y=normal_cord[1]
+        jarvis.click(x,y)
+        jarvis.press('pagedown')
+        jarvis.press('enter')
+Skolaro_ExtraLarge()
