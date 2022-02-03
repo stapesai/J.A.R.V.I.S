2ff69abe21c88cfc6ee11fcf8e6ac9a057f4fc73
Devasheesh Mishra <72092910+devasheeshG@users.noreply.github.com>
2022-02-03 14:42:58 +0530

    .

--- features/download-videos/main.py
+++ features/download-videos/main.py
@@ -1,9 +1,9 @@
 # Importing Modules...
 import csv
-import youtube_dl
+import youtube_dl       # install location is c:\users\swast\appdata\local\programs\python\python310\lib\site-packages\youtube_dl
 
 ydl_opts = {
     'nopart': True,
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://44-fte.divas.cloud/CHAN-5231/CHAN-5231_1.stream/playlist.m3u8'])
+    ydl.download(['https://44-fte.divas.cloud/CHAN-5231/CHAN-5231_1.stream/playlist.m3u8']) 
