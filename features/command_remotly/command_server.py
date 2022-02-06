45b9232efdcacac5c15d6952dd8d3aa7ed984988
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-02-06 19:03:50 +0530

    Update command_server.py

--- features/command_remotly/command_server.py
+++ features/command_remotly/command_server.py
@@ -1,33 +1,25 @@
+
 def send_command(command):
     import time
     import socket
     import sys
     import os
 
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    # s.timeout(0.1)
-    host = socket.gethostname()
-    print("Host: " + host)
-    port = 80
+    s = socket.socket()
+    host = '192.168.1.5'
+    port = 9999
+    
     command = input("Enter command: ")
 
     try:
         s.bind((host, port))
         print("Listening for connections...")
-        s.listen(5)
+        s.listen(1)
         conn, addr = s.accept()
         print("Connected to: " + str(addr))
         
         conn.send(command.encode())
         print("Command sent: " + command)
-        data = conn.recv(1024)
-        if data:
-            print("Received data: " + data.decode())
     except:
         print("Error connecting to client..retrying")
     s.close()
-
-while True:
-    # command = input("Enter command: ")
-    command = 'temp'
-    send_command(command)
